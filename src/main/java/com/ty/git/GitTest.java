b9351944a7b7454e00add8afae5d09a33d92d314
Ty <853375443@qq.com>
2022-11-11 10:07:08 +0800

    second commit

--- src/main/java/com/ty/git/GitTest.java
+++ src/main/java/com/ty/git/GitTest.java
@@ -7,5 +7,6 @@ package com.ty.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
