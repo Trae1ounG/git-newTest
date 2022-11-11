8b3fec3027869186308b6284c2e6b05c18588c28
Ty <853375443@qq.com>
2022-11-11 10:05:38 +0800

    first commit

---
+++ src/main/java/com/ty/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ty.git;
+
+/**
+ * @author ty
+ * @create 2022-11-11 10:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
